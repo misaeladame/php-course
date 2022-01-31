0ddec7179c923c555f4701813e0840dc0c408e14
Misael Adame <misael_adame@protonmail.com>
2022-01-30 21:52:17 -0600

    Operadores ternarios

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
     $a = 5;
-    $b = 3;
+    $b = 5;
 
+    /*
     if ($a > $b) {
         echo "El valor de a es mayor que el valor de b";
     } else if ($b < $b){
@@ -10,5 +11,10 @@
     } else {
         echo "El valor de a es igual que el valor de b";
     }
+    */
+
+    $valor = $a > $b ? 'mayor' : ($a < $b ? 'menor' : 'igual');
+
+    echo "El valor de a es $valor que el valor de b";
 ?>
 
