1d7a2a8b72438d24f0c32a45d300157a73869453
Misael Adame <misael_adame@protonmail.com>
2022-01-30 21:46:49 -0600

    Estructuras condicionales

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 
-    $a = 10;
-    $b = 5;
+    $a = 5;
+    $b = 3;
 
-    $a %= $b;
-
-    echo $a;
+    if ($a > $b) {
+        echo "El valor de a es mayor que el valor de b";
+    } else if ($b < $b){
+        echo "El valor de a es menor que el valor de b";
+    } else {
+        echo "El valor de a es igual que el valor de b";
+    }
 ?>
 
