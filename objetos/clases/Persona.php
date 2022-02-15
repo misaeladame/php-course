5747d50e6688e5642f51dee8767ed3a74937915a
Misael Adame <misael_adame@protonmail.com>
2022-02-15 14:11:29 -0600

    Herencia

--- objetos/clases/Persona.php
+++ objetos/clases/Persona.php
@@ -2,15 +2,16 @@
 
 class Persona
 {
-    private $nombre, $apellido, $edad;
+    public $nombre, $edad;
+    public $apellido1, $apellido2;
 
-    public function __construct($nombre, $apellido, $edad)
-    {
-        $this->nombre = strtolower($nombre);
-        $this->apellido = strtolower($apellido);
-        $this->edad = $edad;
+    // public function __construct($nombre, $apellido, $edad)
+    // {
+    //     $this->nombre = strtolower($nombre);
+    //     $this->apellido = strtolower($apellido);
+    //     $this->edad = $edad;
 
-    }
+    // }
     
     public function setNombre($nombre) {
         $this->nombre = strtolower($nombre);
@@ -19,5 +20,35 @@ class Persona
     public function getNombre() {
         return ucwords($this->nombre);
     }
-    
+
+    public function setApellidos($apellido1, $apellido2) {
+        $this->apellido1 = $apellido1;
+        $this->apellido2 = $apellido2;
+    }
+
+    public function getApellidos() {
+        return $this->apellido1 . " " . $this->apellido2;
+    }
+
+}
+
+class Peruano extends Persona {
+    public $departamento, $ciudad;
+
+    public function setApellidos($apellido1, $apellido2)
+    {
+        parent::setApellidos($apellido1, $apellido2);
+
+        echo "Los apellidos se asignaron con éxito";
+    }
+}
+
+class Chileno extends Persona {
+    public $comuna, $region;
+
+    public function setApellidos($apellido1, $apellido2) {
+        $this->apellido1 = $apellido2;
+        $this->apellido2 = $apellido1;
+    }
+
 }
--- objetos/index.php
+++ objetos/index.php
@@ -2,33 +2,55 @@
 
 require_once('clases/Persona.php');
 
-$persona1 = new Persona("José Misael", "Adame Sandoval", 22);
+// $persona1 = new Persona("José Misael", "Adame Sandoval", 22);
 
 
-// $persona1->nombre = "Misael";
-// $persona1->apellido = "Adame";
-// $persona1->edad = 22;
+// // $persona1->nombre = "Misael";
+// // $persona1->apellido = "Adame";
+// // $persona1->edad = 22;
 
-echo "El nombre de la persona 1 es: " . $persona1->getNombre();
-echo "<br>";
-
-$persona1->setNombre("Misael");
-// $persona1->apellido = "Adame Sandoval";
-// $persona1->edad = 22;
-
-
-$persona2 = new Persona("Luis Alberto", "García Aspe", 50);
-
-// $persona2->setNombre("Luis Alberto");
-// $persona2->apellido = "García Aspe";
-// $persona2->edad = 50;
-
-// var_dump($persona1);
-
-// echo "El nombre de la persona 1 es: " . $persona1->nombre;
+// echo "El nombre de la persona 1 es: " . $persona1->getNombre();
 // echo "<br>";
-// echo "El nombre de la persona 2 es: " . $persona2->nombre;
 
-echo "El nombre de la persona 1 es: " . $persona1->getNombre();
+// $persona1->setNombre("Misael");
+// // $persona1->apellido = "Adame Sandoval";
+// // $persona1->edad = 22;
+
+
+// $persona2 = new Persona("Luis Alberto", "García Aspe", 50);
+
+// // $persona2->setNombre("Luis Alberto");
+// // $persona2->apellido = "García Aspe";
+// // $persona2->edad = 50;
+
+// // var_dump($persona1);
+
+// // echo "El nombre de la persona 1 es: " . $persona1->nombre;
+// // echo "<br>";
+// // echo "El nombre de la persona 2 es: " . $persona2->nombre;
+
+// echo "El nombre de la persona 1 es: " . $persona1->getNombre();
+// echo "<br>";
+// echo "El nombre de la persona 2 es: " . $persona2->getNombre();
+
+$peruano = new Peruano;
+$peruano->setNombre("Victor Hernán");
+
+$chileno = new Chileno;
+
+$chileno->setApellidos("Adame", "Sandoval");
+
+echo $chileno->getApellidos();
 echo "<br>";
-echo "El nombre de la persona 2 es: " . $persona2->getNombre();
+
+$peruano = new Peruano;
+
+$peruano->setApellidos("Adame", "Sandoval");
+
+echo "<br>";
+
+echo $peruano->getApellidos();
+
+echo "<br>";
+
+var_dump($peruano);
