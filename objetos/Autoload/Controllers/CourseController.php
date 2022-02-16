45fe49aa04205bffd1dbc1b46bf3a46e1ceb9f47
Misael Adame <misael_adame@protonmail.com>
2022-02-15 19:01:28 -0600

    AutoLoad

---
+++ objetos/Autoload/Controllers/CourseController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Controllers;
+
+class CourseController {
+    public function saludar() {
+        echo "Saludo desde la clase course controller";
+    }
+}
--- objetos/Autoload/index.php
+++ objetos/Autoload/index.php
@@ -1,2 +1,20 @@
 <?php
 
+use Controllers\CourseController;
+use Models\Course;
+
+spl_autoload_register(function($clase) {
+    //echo str_replace('\\', '/', $clase) . '.php';
+    if (file_exists(str_replace('\\', '/', $clase) . '.php')) {
+        require_once(str_replace('\\', '/', $clase) . '.php');
+    }
+    
+});
+
+$course = new Course;
+$course->saludar();
+
+echo "<br>";
+
+$course_controller = new CourseController;
+$course_controller->saludar();
